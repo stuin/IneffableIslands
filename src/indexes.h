7dce3e9653e62fc280e504cbc7b25d0fcbf31a11
stuin <stuin01@gmail.com>
2025-01-06 19:15:04 -0800

    Texture cleanup

--- src/indexes.h
+++ src/indexes.h
@@ -4,6 +4,7 @@
 
 enum CollisionLayer {
 	MAP,
+	WATER,
 	PLAYER,
 	TREES,
 	INPUT,
@@ -12,20 +13,45 @@ enum CollisionLayer {
 static const std::map<int, int> displayIndex = {
 	{' ', 0},
 	{'~', 0},
-	{',', 1},
-	{'.', 2},
-	{'t', 2}
+	{'w', 2},
+	{'W', 2},
+	{',', 0},
+	{'.', 1},
+	{'_', 2},
+	{'-', 3},
+	{'t', 1},
+	{'T', 2}
+};
+
+static const std::map<int, int> rotationIndex = {
+	{0, 4},
+	{1, 4},
+	{2, 4},
+	{3, 4}
 };
 
 static const std::map<int, int> collisionIndex = {
 	{' ', FULL},
 	{'~', FULL},
+	{'w', FULL},
+	{'W', FULL},
 	{',', EMPTY},
 	{'.', EMPTY},
-	{'t', FULL}
+	{'_', EMPTY},
+	{'-', EMPTY},
+	{'t', FULL},
+	{'T', FULL}
 };
 
 static const std::map<int, int> treetopIndex = {
 	{' ', -1},
-	{'t', 0}
+	{'t', 1},
+	{'T', 0}
+};
+
+static const std::map<int, int> waterIndex = {
+	{' ', -1},
+	{'~', 0},
+	{'w', 0},
+	{'W', 1}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <SFML/Graphics.hpp>
 
 #include "Skyrmion/tiling/TileMap.hpp"
+#include "Skyrmion/tiling/TileFilters.hpp"
 #include "indexes.h"
 #include "Player.hpp"
 
@@ -12,42 +13,48 @@ int main() {
 
 	//Load tilemap textures
 	sf::Texture beachTexture;
-	sf::Texture treemidTexture;
-	sf::Texture treetopTexture;
+	sf::Texture treeTexture;
+	sf::Texture waterTexture;
 	UpdateList::loadTexture(&beachTexture, "res/beachtiles.png");
-	UpdateList::loadTexture(&treemidTexture, "res/treemid.png");
-	UpdateList::loadTexture(&treetopTexture, "res/treetop.png");
+	UpdateList::loadTexture(&treeTexture, "res/trees.png");
+	UpdateList::loadTexture(&waterTexture, "res/water.png");
 
 	//Load base tile maps
 	GridMaker grid("res/test_island.txt");
-	TileMap beach(&beachTexture, 16, 16, new MapIndexer(&grid, displayIndex, 0), MAP);
+	Indexer *beachIndexer = new RandomIndexer(new MapIndexer(&grid, displayIndex, 0), rotationIndex, 0, 4);
+	TileMap beach(&beachTexture, 16, 16, beachIndexer, MAP);
 	MapIndexer collisionMap(&grid, collisionIndex, 0, 16, 16);
 	MapIndexer treetopMap(&grid, treetopIndex, -1);
+	MapIndexer waterMap(&grid, waterIndex, -1);
 	UpdateList::addNode(&beach);
 
 	//Add overlapping tree middles
-	TileMap treemid(&treemidTexture, 16, 16, &treetopMap, TREES);
+	TileMap treemid(&treeTexture, 16, 16, &treetopMap, TREES, 2);
 	treemid.setPosition(0, -4);
 	UpdateList::addNode(&treemid);
 
 	//Add overlapping tree tops
-	TileMap treetop(&treetopTexture, 16, 16, &treetopMap, TREES);
+	TileMap treetop(&treeTexture, 16, 16, &treetopMap, TREES);
 	treetop.setPosition(0, -20);
 	UpdateList::addNode(&treetop);
 
+	//Add animated water texture
+	AnimatedTileMap water(&waterTexture, 16, 16, &waterMap, 4, 0.8, WATER);
+	UpdateList::addNode(&water);
+
 	//Load node textures
 	sf::Texture playerTexture;
-	sf::Texture treasureTexture;
 	UpdateList::loadTexture(&playerTexture, "res/player.png");
 
-	//Upper area player
+	//Player
 	Player player(&collisionMap);
-	player.setPosition(sf::Vector2f(7*16, 6*16));
+	player.setPosition(sf::Vector2f(16*16, 16*16));
 	player.setTexture(playerTexture);
 	UpdateList::addNode(&player);
 
 	//Finish engine setup
 	UpdateList::staticLayer(MAP);
+	UpdateList::staticLayer(PLAYER);
 	UpdateList::staticLayer(TREES);
 	UpdateList::staticLayer(INPUT);
 	UpdateList::setCamera(&player, sf::Vector2f(450, 250));
